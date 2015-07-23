8cd2f23ed8a3d3d33e4c892fac9705c379129662
Christopher Agostino <cjp.agostino@gmail.com>
2015-07-23 20:14:56 +0000

    xmcd.py edited online with Bitbucket

--- xmcd.py
+++ xmcd.py
@@ -1,6 +1,9 @@
 #lets gooooo. Author: Christopher Agostino
 import numpy as np
 import os
+import osgeo
+import scipy
+import gdal
 import matplotlib.pyplot as plt
 from math import *
 from matplotlib.backend_bases import MouseEvent
@@ -12,12 +15,21 @@ def assign_images(lst = None):
 		if fil.endswith(".fits"):
 			new_array.append(pyfits.open(fil)[0].data)
 			file_list.append(fil)
+		elif fil.endswith(".tif"):
+			new =gdal.Open(fil)
+
+			new_array.append(scipy.array(new.GetRasterBand(1).ReadAsArray()))
+			file_list.append(fil)
 	return [file_list, new_array]
 list_images = assign_images()[1]
 list_files = assign_images()[0]
 photon_energies = []
 for i in list_files:
-	i = i.replace(".fits","")
+	if 'fits' in i:
+
+		i = i.replace(".fits","")
+	if 'tif' in i:
+		i = i.replace("_L.tif","")
 	photon_energies.append(float(i))
 def pick_domain_wall_point(image):
 	plt.imshow(image,cmap='Greys_r')
@@ -45,7 +57,7 @@ def calculate_nth_average_intensity(image,coordinate, size, number):
                         intensities_left.append(image[coordinate[1]+count_y][coordinate[0]+count_x-size*(number-1)])
                         count_y+=1
                 count_x -=1
-	return [-np.sum(intensities_left)/(abs(count_x)*count_y)+np.sum(intensities_right)/(abs(count_x)*count_y), np.sum(intensities_left)/(abs(count_x)*count_y), np.sum(intensities_right)/(abs(count_x)*count_y) ]
+	return [np.sum(intensities_left)/(abs(count_x)*count_y)-np.sum(intensities_right)/(abs(count_x)*count_y), np.sum(intensities_left)/(abs(count_x)*count_y), np.sum(intensities_right)/(abs(count_x)*count_y) ]
 #point1=[153,774]
 point =[int(i) for i in list(raw_input("enter an x and y coordinate: ").split(','))]
 pixel_list = [5,8,10,15,20]
@@ -66,4 +78,5 @@ def producedata(image_list, pixel_list,number):
 			for k in range(len(photon_energies)):
 				fileref.write(str(photon_energies[k]) + " " + str(intens[k]) + " " + str(intensleft[k]) +" " +str(intensright[k]) +"\n")
 			fileref.close()
+
 producedata(list_images,pixel_list,5	)
